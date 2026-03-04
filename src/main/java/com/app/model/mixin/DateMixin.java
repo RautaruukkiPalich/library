3fc97d5efe2603c62cc79878dea395055667922f
palich <rautaruukkipalich@gmail.com>
2026-03-04 12:09:19 +0500

    test workflow

--- src/main/java/com/app/model/mixin/DateMixin.java
+++ src/main/java/com/app/model/mixin/DateMixin.java
@@ -6,7 +6,6 @@ import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
-import org.springframework.beans.factory.annotation.Value;
 
 import java.time.OffsetDateTime;
 
@@ -14,16 +13,15 @@ import java.time.OffsetDateTime;
 @Setter
 @MappedSuperclass
 public class DateMixin {
-    //    private final static String DEFAULT = "TIMESTAMPTZ NOT NULL DEFAULT now()";
-    @Value("${app.db.timestamp.definition:TIMESTAMPTZ NOT NULL DEFAULT now()}")
-    private String timestampDefinition;
+
+    private final static String DEFAULT = "TIMESTAMPTZ NOT NULL DEFAULT now()";
 
     @CreationTimestamp
-    @Column(name = "created_at", nullable = false, updatable = false, columnDefinition = "#{@dateMixin.timestampDefinition}")
+    @Column(name = "created_at", nullable = false, updatable = false, columnDefinition =DEFAULT)
     private OffsetDateTime createdAt;
 
     @UpdateTimestamp
-    @Column(name = "updated_at", nullable = false, columnDefinition = "#{@dateMixin.timestampDefinition}")
+    @Column(name = "updated_at", nullable = false, columnDefinition = DEFAULT)
     private OffsetDateTime updatedAt;
 
     protected DateMixin() {
