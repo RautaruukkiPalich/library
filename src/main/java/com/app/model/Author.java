e5b659380cec05ec6f1d797c96dcda023318589a
palich <rautaruukkipalich@gmail.com>
2026-03-05 19:27:08 +0500

    refactor: refactor Author validation

--- src/main/java/com/app/model/Author.java
+++ src/main/java/com/app/model/Author.java
@@ -3,11 +3,14 @@ package com.app.model;
 import com.app.dto.AuthorDTO;
 import com.app.exception.validation.AuthorValidationException;
 import com.app.model.mixin.DateMixin;
+import com.app.utils.mapUtils.MapUtils;
+import com.app.utils.validator.Validator;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 
 @Getter
@@ -19,17 +22,23 @@ public class Author extends DateMixin {
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
 
-    @Column(nullable = false, length = 100)
+    @Column(nullable = false, length = 255)
     private String firstname;
 
-    @Column(nullable = false, length = 100)
+    @Column(nullable = false, length = 255)
     private String lastname;
 
-    @Column(nullable = false, length = 100)
+    @Column(nullable = false, length = 255)
     private String surname;
 
     @OneToMany(mappedBy = "author", cascade = CascadeType.ALL)
     private List<Book> books;
+
+    private static final String ID_KEY = "id";
+    private static final String FIRSTNAME_KEY = "firstname";
+    private static final String LASTNAME_KEY = "lastname";
+    private static final String SURNAME_KEY = "surname";
+
 
     public Author() {
         super();
@@ -56,60 +65,44 @@ public class Author extends DateMixin {
     }
 
     public void validate() throws AuthorValidationException {
-        this.validateFirstname();
-        this.validateLastname();
-        this.validateSurname();
+        collectValidates(
+                this.validateFirstname(),
+                this.validateLastname(),
+                this.validateSurname()
+        );
     }
 
     public void validateStrict() throws AuthorValidationException {
-        if (this.getId() == null) {
-            throw new AuthorValidationException("id", "cant be less than 1");
-        }
-        this.validate();
+        collectValidates(
+                this.validateId(),
+                this.validateFirstname(),
+                this.validateLastname(),
+                this.validateSurname()
+        );
     }
 
-    private void validateFirstname() throws AuthorValidationException {
-        if (this.firstname == null) {
-            throw new AuthorValidationException("firstname", "cant be null");
+    @SafeVarargs
+    private void collectValidates(Map<String, String>... maps) throws AuthorValidationException {
+        var errors = MapUtils.merge(maps);
+        if (errors == null || errors.isEmpty()) {
+            return;
         }
-        if (this.firstname.isBlank()) {
-            throw new AuthorValidationException("firstname", "cant be blank");
-        }
-        if (this.firstname.length() < 2) {
-            throw new AuthorValidationException("firstname", "cant be shorter 2 characters");
-        }
-        if (this.firstname.length() > 255) {
-            throw new AuthorValidationException("firstname", "cant be longer 255 characters");
-        }
+        throw new AuthorValidationException(errors);
     }
 
-    private void validateLastname() throws AuthorValidationException {
-        if (this.lastname == null) {
-            throw new AuthorValidationException("lastname", "cant be null");
-        }
-        if (this.lastname.isBlank()) {
-            throw new AuthorValidationException("lastname", "cant be blank");
-        }
-        if (this.lastname.length() < 2) {
-            throw new AuthorValidationException("lastname", "cant be shorter 2 characters");
-        }
-        if (this.lastname.length() > 255) {
-            throw new AuthorValidationException("lastname", "cant be longer 255 characters");
-        }
+    private Map<String, String> validateId() {
+        return Validator.validateLong(ID_KEY, this.id, 1L, null);
     }
 
-    private void validateSurname() throws AuthorValidationException {
-        if (this.surname == null) {
-            throw new AuthorValidationException("surname", "cant be null");
-        }
-        if (this.surname.isBlank()) {
-            throw new AuthorValidationException("surname", "cant be blank");
-        }
-        if (this.surname.length() < 2) {
-            throw new AuthorValidationException("surname", "cant be shorter 2 characters");
-        }
-        if (this.surname.length() > 255) {
-            throw new AuthorValidationException("surname", "cant be longer 255 characters");
-        }
+    private Map<String, String> validateFirstname() {
+        return Validator.validateString(FIRSTNAME_KEY, this.firstname, 2, 255);
+    }
+
+    private Map<String, String> validateLastname() {
+        return Validator.validateString(LASTNAME_KEY, this.lastname, 2, 255);
+    }
+
+    private Map<String, String> validateSurname() {
+        return Validator.validateString(SURNAME_KEY, this.surname, 2, 255);
     }
 }
